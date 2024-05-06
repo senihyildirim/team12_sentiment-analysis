3966076a53a00a817b86c10976ffc81e42916295
Senih YILDIRIM <74868914+senihyildirim@users.noreply.github.com>
2024-05-06 22:31:37 +0300

    Update model.py
    cnn model development labelencoder and tokenizer saved

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Conv1D, GlobalMaxPooling1D, Dense
 from tensorflow.keras.callbacks import ModelCheckpoint
+import pickle  # For saving tokenizer and label encoder
 
 # Ensure the 'punkt' tokenizer model is available
 nltk.download('punkt')
@@ -28,10 +29,18 @@ tokenizer.fit_on_texts(df['text'].values)
 X = tokenizer.texts_to_sequences(df['text'].values)
 X = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
 
+# Save tokenizer
+with open('saved_model/tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 # Encoding categorical labels
 labelencoder = LabelEncoder()
 integer_encoded = labelencoder.fit_transform(df['label'])
 y = to_categorical(integer_encoded)
+
+# Save label encoder
+with open('saved_model/labelencoder.pickle', 'wb') as handle:
+    pickle.dump(labelencoder, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 # Splitting data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=40)
@@ -54,7 +63,7 @@ filepath = "saved_model/model-{epoch:02d}-{val_accuracy:.2f}.keras"
 checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
 
 # Train the model
-epochs = 10
+epochs = 50
 batch_size = 64
 history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test), callbacks=[checkpoint])
 
@@ -63,4 +72,4 @@ test_loss, test_acc = model.evaluate(X_test, y_test)
 print('Test Accuracy:', test_acc)
 
 # Save the final model
-model.save('saved_model/final_model.keras')  
+model.save('saved_model/final_model.keras')
